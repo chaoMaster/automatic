054ef3c9b917816fec07b2493566af0c425c3698
chaochao <chaochao@fake.local>
2017-11-27 15:45:41 +0800

    version 2.1

--- automaticRemedy.py
+++ automaticRemedy.py
@@ -1,13 +1,18 @@
-# -*- coding: GBK -*-
+# -*- coding: UTF-8 -*-
 
 from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import ElementNotVisibleException
+from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
 import time
+
+import json
+import urllib2
 
 import sys
 import getopt
@@ -17,15 +22,96 @@ import subprocess
 import threading
 import platform
 
-def NetCheck(ip):   #¼ì²âÍøÂç×´¿ö
+
+opts, args = getopt.getopt(sys.argv[1:], "hc:")
+username = ""
+password = ""
+driverpath = ""
+
+
+slackAPP_controlRemedyScriptNetworkListenSwitch = "http://124.251.110.252/SlackAPP/slackEventListen/controlRemedyScriptNetworkListenSwitch"
+slackAPP_setRemedyNetListenTrue = "http://124.251.110.252/SlackAPP/slackEventListen/setRemedyNetListenTrue"
+# netMonitoringUrl = "127.0.0.1"
+
+configPath = "config.ini"
+
+# for op, value in opts:
+#     if op == "-c":
+#         configPath = value
+#     elif op == "-h":
+#         print "-c -------- config file path"
+#         print ""
+#         print "Please download the chrome driver before use"
+#         print "driver download and version, CSDN link ï¼šhttp://blog.csdn.net/chaomaster/article/details/52963265"
+#         print "when the script exit ,we must log out the remedy"
+#         print "Please ensure that the parameters of the input is correct, otherwise may cause driver abnormal or logon failure"
+#         sys.exit()
+
+# if not configPath:
+#     print "ERR : no configuration \n"
+#     print "please create configuration file\n"
+#     print ""
+#     print "Please download the chrome driver before use"
+#     print "driver download and version, CSDN link ï¼šhttp://blog.csdn.net/chaomaster/article/details/52963265"
+#     print "when the script exit ,we must log out the remedy"
+#     print "Please ensure that the configuration file's parameters of the input is correct, otherwise may cause driver abnormal or logon failure"
+#     sys.exit()
+
+
+
+
+try:
+    config = ConfigParser.ConfigParser()
+    with open(configPath, 'r+') as cfgfile:
+        config.readfp(cfgfile)
+
+    username = config.get("info", "username")
+    password = config.get("info", "password")
+    driverpath = config.get("info", "driverpath")
+    audioPath = config.get("info", "audiopath")
+    slack_channel = config.get("info", "slack_channel")
+    slackAPP_postMessageAPI = config.get("info", "slackAPP_postMessageAPI")
+    slackApp_postUser = config.get("info", "slackApp_postUser")
+except:
+    print "ERR : no configuration \n"
+    print "please create configuration file\n"
+    print "configuration must be renamed config.ini\n"
+    print "configuration need to fill username .password .driverPath .audioPath\n"
+    print "driver download and version, CSDN link : http://blog.csdn.net/chaomaster/article/details/52963265\n"
+    print "the script will be exit in 5 second\n"
+    time.sleep(5)
+
+if (not username) or (not password) or (not driverpath):
+    print "Please send the user name, password, drive path to configuration file, such as config.ini "
+    sys.exit()
+
+
+def postSlackAPP(requestMessage): #è°ƒç”¨ slack æ¥å£æ–¹æ³•
+    headers = {'Content-Type': 'application/json'}
+    request = urllib2.Request(url=slackAPP_postMessageAPI, headers=headers, data=json.dumps(requestMessage))
+    response = urllib2.urlopen(request)
+    return "å‘é€æˆåŠŸ"
+
+def postNetSwitch(postMessage):
+    header = {'Content-Type': 'application/json'}
+    netRequest = urllib2.Request(url=slackAPP_controlRemedyScriptNetworkListenSwitch, headers=header, data=json.dumps(postMessage))
+    response = urllib2.urlopen(netRequest)
+
+def postNetStatus(postMessage):
+    header = {'Content-Type': 'application/json'}
+    netRequest = urllib2.Request(url=slackAPP_setRemedyNetListenTrue, headers=header,
+                                 data=json.dumps(postMessage))
+    response = urllib2.urlopen(netRequest)
+
+def NetCheck(ip):   #æ£€æµ‹ç½‘ç»œçŠ¶å†µ
     try:
         if platform.system() == "Windows":
-            # print "ÕıÈ·½øÈë windows ·ÖÖ§"
+            # print "æ­£ç¡®è¿›å…¥ windows åˆ†æ”¯"
             p = subprocess.Popen(["ping", "-n", "4", ip],stdout=subprocess.PIPE, stderr=subprocess.PIPE)
             outW = p.stdout.read()
             errW = p.stderr.read()
             regex1 = re.compile("100%")
-            regex2 = re.compile("ÕÒ²»µ½Ö÷»ú")
+            regex2 = re.compile("æ‰¾ä¸åˆ°ä¸»æœº")
             # print ""
             # print "outW"
             # print outW
@@ -59,20 +145,28 @@ def NetCheck(ip):   #¼ì²âÍøÂç×´¿ö
 
 def threadOffline(threadName, delay):
     i = 1
+    netMonitoringStart = {"channel": "#operations", "user": slackApp_postUser, "swi": "open"}
+    postNetSwitch(netMonitoringStart)
     while True:
-        time.sleep(180)
+        time.sleep(120)
         flag1 = NetCheck("www.baidu.com")
         flag2 = NetCheck("chinabluemix.itsm.unisysedge.cn")
+        netMonitoringContinue = {"bo": "true"}
+        postNetStatus(netMonitoringContinue)
         # print flag
         # print i
         # i += 1
         if (flag1 == False) and (flag2 == False):
-            print u"ÍøÂçÁ¬½ÓÊ§°Ü"
+            print u"ç½‘ç»œè¿æ¥å¤±è´¥"
+            netErrorNC = {"channel": "#operations", "text": slackApp_postUser + "   ç½‘ç»œå¼‚å¸¸ï¼Œè¯·æ³¨æ„ï¼ï¼ï¼"}
+            postSlackAPP(netErrorNC)
+            # netMonitoringErr = {"status": "err"}
+            # postNetMonitoring(netMonitoringErr)
             offlineBrowser = webdriver.Chrome(executable_path=driverpath)
             offlineBrowser.get(audioPath)
             break
 
-# def isElementExist(element):  # ÅĞ¶Ïº¯Êı
+# def isElementExist(element):  # åˆ¤æ–­å‡½æ•°
 #     flag = True
 #     try:
 #         browser.find_element_by_xpath(element)
@@ -83,129 +177,135 @@ def threadOffline(threadName, delay):
 
 
 
-opts, args = getopt.getopt(sys.argv[1:], "hc:")
-username = ""
-password = ""
-driverpath = ""
 
-configPath = "config.ini"
-
-# for op, value in opts:
-#     if op == "-c":
-#         configPath = value
-#     elif op == "-h":
-#         print "-c -------- config file path"
-#         print ""
-#         print "Please download the chrome driver before use"
-#         print "driver download and version, CSDN link £ºhttp://blog.csdn.net/chaomaster/article/details/52963265"
-#         print "when the script exit ,we must log out the remedy"
-#         print "Please ensure that the parameters of the input is correct, otherwise may cause driver abnormal or logon failure"
-#         sys.exit()
-
-# if not configPath:
-#     print "ERR : no configuration \n"
-#     print "please create configuration file\n"
-#     print ""
-#     print "Please download the chrome driver before use"
-#     print "driver download and version, CSDN link £ºhttp://blog.csdn.net/chaomaster/article/details/52963265"
-#     print "when the script exit ,we must log out the remedy"
-#     print "Please ensure that the configuration file's parameters of the input is correct, otherwise may cause driver abnormal or logon failure"
-#     sys.exit()
-
-try:
-    config = ConfigParser.ConfigParser()
-    with open(configPath, 'r+') as cfgfile:
-        config.readfp(cfgfile)
-
-    username = config.get("info", "username")
-    password = config.get("info", "password")
-    driverpath = config.get("info", "driverpath")
-    audioPath = config.get("info", "audiopath")
-except:
-    print "ERR : no configuration \n"
-    print "please create configuration file\n"
-    print "configuration must be renamed config.ini\n"
-    print "configuration need to fill username .password .driverPath .audioPath\n"
-    print "driver download and version, CSDN link : http://blog.csdn.net/chaomaster/article/details/52963265\n"
-    print "the script will be exit in 5 second\n"
-    time.sleep(5)
-
-if (not username) or (not password) or (not driverpath):
-    print "Please send the user name, password, drive path to configuration file, such as config.ini "
-    sys.exit()
 
 def threadMain(threadName, delay):
 
 
-# Çı¶¯ÅäÖÃ¼°ÏÂÔØ CSDN Á¬½Ó £ºhttp://blog.csdn.net/chaomaster/article/details/52963265
+# é©±åŠ¨é…ç½®åŠä¸‹è½½ CSDN è¿æ¥ ï¼šhttp://blog.csdn.net/chaomaster/article/details/52963265
 
 # browser = webdriver.Firefox(executable_path='/Users/xuechao/seleniumSupport/geckodriver')
     browser = webdriver.Chrome(executable_path=driverpath)
 
     browser.get('https://chinabluemix.itsm.unisysedge.cn/arsys/shared/loggedout.jsp')
-    browser.implicitly_wait(30)
+    browser.implicitly_wait(50)
 
 
-# µÇÂ¼ remedy
+# ç™»å½• remedy
     browser.find_element_by_id('username-id').send_keys(username)
     browser.find_element_by_id('pwd-id').send_keys(password)
     browser.find_element_by_id('loginText').click()
 
 
-# ´¦ÀíÁ÷³Ì
-    locator1 = (By.XPATH, ".//*[@id='WIN_1_304017100']/div/div")  # ¶¨Î»ÒÑÖ¸ÅÉÒ³Ãæ°´Å¥
-    locator2 = (By.CSS_SELECTOR, ".BaseTableCellOdd.BaseTableCellOddColor.BaseTableStaticText")  # ¶¨Î» ticket
+# å¤„ç†æµç¨‹
+    locator1 = (By.XPATH, ".//*[@id='WIN_1_304017100']/div/div")  # å®šä½å·²æŒ‡æ´¾é¡µé¢æŒ‰é’®
+    locator2 = (By.CSS_SELECTOR, ".BaseTableCellOdd.BaseTableCellOddColor.BaseTableStaticText")  # å®šä½ ticket
 
-# Ìí¼ÓËÀÑ­»·
+# æ·»åŠ æ­»å¾ªç¯
 
-    i = 1  # ¼ÆÊı
+    i = 1  # è®¡æ•°
 
     enTestString = "Test"
     enTestString2 = "Test"
     enTestString3 = "TEST"
-    cnTestString = u"²âÊÔ"
+    cnTestString = u"æµ‹è¯•"
 
     while True:
         try:
-            WebDriverWait(browser, 30, 0.5).until(EC.visibility_of_element_located(locator1))
+            WebDriverWait(browser, 50, 0.5).until(EC.visibility_of_element_located(locator1))
             browser.find_element_by_xpath(".//*[@id='WIN_1_304017100']/div/div").click()
 
-        finally:
-            try:
-                try:
-                    WebDriverWait(browser, 30000000, 0.5).until(EC.visibility_of_element_located(locator2))  # Ä£ÄâµÈ´ı Ê±¼äÎŞÏŞ´ó
+        # except NoSuchElementException as e:
+        #     try:
+        #         browser.switch_to_alert().accept()
+        #
 
-                    testString = browser.find_element_by_xpath(".//*[@id='T302087200']/tbody/tr[2]/td[2]/nobr/span").text # ¹ıÂË ²âÊÔ »òÕß test
-                    if ((enTestString in testString) == False) and ((cnTestString in testString) == False) and ((enTestString2 in testString) == False) and ((enTestString3 in testString) == False):
-                        doubleClickArea = browser.find_element_by_css_selector(".BaseTableCellOdd.BaseTableCellOddColor.BaseTableStaticText")
-                        ActionChains(browser).double_click(doubleClickArea).perform()
-                    else:
-                        print "¡¾test¡¿ ticket, please manual processing"
-                        alertBrowser = webdriver.Chrome(executable_path=driverpath)
-                        alertBrowser.get(audioPath)
-                finally:
-                    try:
-                        browser.find_element_by_xpath(".//*[@id='arid_WIN_2_536870940']").send_keys("in progress")  # Ìí¼Ó³õÊ¼ÏìÓ¦
+        except:
+            print u"è¿›å…¥æœªå—ç†æ ‡ç­¾å¤±è´¥"
+            # browser.refresh()
+            #     print u"è·å–å…ƒç´ å¤±è´¥ï¼Œè¯·æ­£å¸¸ç™»å‡ºåé‡å¯è„šæœ¬"
+            ackErrorNC = {"channel": "#operations", "text": slackApp_postUser + "   è‡ªåŠ¨å—ç†è„šæœ¬å¼‚å¸¸ï¼ï¼ï¼ˆå¼‚å¸¸è¯¦æƒ…ï¼šæ‰¾ä¸åˆ°æœªå—ç†æ ‡ç­¾ï¼‰"}
+            postSlackAPP(ackErrorNC)
 
-                        browser.find_element_by_xpath(".//*[@id='WIN_2_536870924']/div/div").click()  # ÊÜÀí
-                        print "already deal " + str(i) + " ticket"
-                        print "ticket summary: " + testString
-                        i += 1
-                        time.sleep(2)
-                        browser.refresh()  # ä¯ÀÀÆ÷Ë¢ĞÂ
-                        timeString = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))  # »ñÈ¡µ±Ç°Ê±¼ä
-                        f = open("ticket.log", "a+")
-                        f.read()
-                        f.write(timeString + " " + testString + "\n")
-                        f.close()
-                        time.sleep(3)
-                    except:
-                        browser.refresh()
-            except:
-                print u"»ñÈ¡ÔªËØÊ§°Ü£¬ÇëÕı³£µÇ³öºóÖØÆô½Å±¾"
-                alertBrowser = webdriver.Chrome(executable_path=driverpath)
-                alertBrowser.get(audioPath)
+            # netMonitoringStop = {"status": "stop"}
+            # postNetMonitoring(netMonitoringStop)
+
+            ackBrowser = webdriver.Chrome(executable_path=driverpath)
+            ackBrowser.get(audioPath)
                 # print audioPath
+
+        finally:
+
+            try:
+                WebDriverWait(browser, 30000000, 0.5).until(EC.visibility_of_element_located(locator2))  # æ¨¡æ‹Ÿç­‰å¾… æ—¶é—´æ— é™å¤§
+
+                testString = browser.find_element_by_xpath(".//*[@id='T302087200']/tbody/tr[2]/td[2]/nobr/span").text # è¿‡æ»¤ æµ‹è¯• æˆ–è€… test
+                if ((enTestString in testString) == False) and ((cnTestString in testString) == False) and ((enTestString2 in testString) == False) and ((enTestString3 in testString) == False):
+                    doubleClickArea = browser.find_element_by_css_selector(".BaseTableCellOdd.BaseTableCellOddColor.BaseTableStaticText")
+                    ActionChains(browser).double_click(doubleClickArea).perform()
+                else:
+                    print "ã€testã€‘ ticket, please manual processing"
+                    testBrowser = webdriver.Chrome(executable_path=driverpath)
+                    testBrowser.get(audioPath)
+                    # exit()
+            finally:
+                try:
+                    browser.find_element_by_xpath(".//*[@id='arid_WIN_2_536870940']").send_keys("in progress")  # æ·»åŠ åˆå§‹å“åº”
+
+                    time.sleep(1)
+
+                    # browser.find_element_by_xpath(u".//*[@id='WIN_2_536870930']/div/div").click()  # å—ç† .//*[@id='WIN_2_536870924']/div
+
+                    # ackArea = browser.find_element_by_css_selector("#WIN_2_536870924")
+                    ackArea = browser.find_element_by_xpath(".//*[@id='WIN_2_536870924']/div/div")
+                    # ActionChains(browser).double_click(ackArea).perform()
+                    saveArea = browser.find_element_by_xpath(".//*[@id='WIN_2_301614800']/div/div")
+
+                    saveArea.click()
+
+                    time.sleep(1)
+
+                    ackArea.click()
+
+                    print "already deal " + str(i) + " ticket"
+                    print "ticket summary: " + testString
+                    i += 1
+                    time.sleep(2)
+                    browser.refresh()  # æµè§ˆå™¨åˆ·æ–°
+                    timeString = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))  # è·å–å½“å‰æ—¶é—´
+                    f = open("ticket.log", "a+")
+                    f.read()
+                    f.write(timeString + " " + testString + "\n")
+                    f.close()
+                    time.sleep(3)
+
+
+
+                except:
+                    print u"å—ç†æŒ‰é’®è·å–å¤±è´¥ || æ·»åŠ åˆå§‹å“åº”å¤±è´¥"
+
+                    ackLableErrorNC = {"channel": "#operations", "text": slackApp_postUser + "   æ•è·å¼‚å¸¸ï¼Œè¯·æ£€æŸ¥è„šæœ¬çŠ¶æ€"}
+                    postSlackAPP(ackLableErrorNC)
+
+                    # netMonitoringStop = {"status": "stop"}
+                    # postNetMonitoring(netMonitoringStop)
+
+                    # ackLableBrowser = webdriver.Chrome(executable_path=webdriver)
+                    # ackLableBrowser.get(audioPath)
+                    # exit()
+
+
+
+
+                    browser.refresh()
+
+                    try:   # è­¦å‘Šæ¡†å¤„ç†
+                        browser.switch_to_alert().accept()
+                        print "å·²ç¡®è®¤è­¦å‘Šå¼¹çª—"
+                    except :
+                        print "æœªæ‰¾åˆ°è­¦å‘Šæ¡†"
+                        # print e
+
 
 threads = []
 t1 = threading.Thread(target=threadMain, args=("thread", 0,))
@@ -226,13 +326,13 @@ if __name__ == "__main__":
 #     WebDriverWait(browser, 30, 0.5).until(EC.visibility_of_element_located(locator2))
 #     doubleClickArea = browser.find_element_by_xpath(".//*[@id='T302087200']/tbody/tr[2]/td[@class='BaseTableCellOdd BaseTableCellOddColor BaseTableStaticText'][1]")
 #     ActionChains(browser).double_click(doubleClickArea).perform()
-#     print '»ñÈ¡³É¹¦'
+#     print 'è·å–æˆåŠŸ'
 # finally:
 #     browser.close()
 
 
-# ×¢Ïú remedy
-# print '¿ªÊ¼×¢Ïú'
+# æ³¨é”€ remedy
+# print 'å¼€å§‹æ³¨é”€'
 # browser.find_element_by_xpath('.//*[@id="WIN_0_300000044"]/div/div').click()
 #
 #
